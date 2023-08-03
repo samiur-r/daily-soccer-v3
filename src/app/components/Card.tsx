1f0e39fbd4d4822ecbc38e900beda98ce412c24f
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-03 22:03:21 +0600

    feat: add card component

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+
+interface DataType {
+  Competition: {
+    Name: string;
+  };
+  LocalTeam: {
+    Name: string;
+    Image: string;
+  };
+  AwayTeam: {
+    Name: string;
+    Image: string;
+  };
+  Date: string;
+  Channels: Array<{
+    Name: string;
+    Image: string;
+  }>;
+}
+
+interface CardProps {
+  data: Array<DataType>;
+}
+
+const Card: React.FC<CardProps> = ({ data }) => {
+  return (
+    <div className="flex max-w-4xl shadow-lg border">
+      <div className="w-5 bg-emerald-700" />
+      <div className="p-5 flex-1">
+        <p className="flex justify-end text-xs md:text-base">Manana 18:00</p>
+        <h3 className="text-sm md:text-xl font-semibold italic">
+          Fase regular
+        </h3>
+        <div className="flex gap-3 md:text-3xl font-bold mt-3">
+          <div className="flex gap-2 items-center">
+            <Image src="/barcelona.png" width={30} height={30} alt="icon" />
+            <h1>Fase regular</h1>
+          </div>
+          <p>-</p>
+          <div className="flex gap-2 items-center">
+            <Image src="/barcelona.png" width={30} height={30} alt="icon" />
+            <h1>Fase regular</h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1 md:items-end mt-3">
+          <p>M+ Liga de Campeones 2 (M57-O116)</p>
+          <p>M+ #Vamos Bar 2 (305)</p>
+        </div>
+        <div className="flex gap-2 md:justify-end mt-5">
+          <Image src="/moviestar.png" width={80} height={45} alt="icon" />
+          <Image src="/moviestar.png" width={80} height={45} alt="icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen m-3 md:ml-80">
-      Content
+    <main className="flex flex-col gap-5 min-h-screen m-3 md:ml-80">
+      <Card />
     </main>
-  )
+  );
 }
