bcbe8f185fdd69b46a4fd3958291e0cadf065c62
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-05 01:56:42 +0600

    chore: update socket endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     const ws = new WebSocket(
       process.env.NEXT_PUBLIC_ENV === "development"
         ? `ws://localhost:3000`
-        : `ws://${process.env.NEXT_PUBLIC_API_DOMAIN}`
+        : `wss://${process.env.NEXT_PUBLIC_API_DOMAIN}`
     );
 
     ws.onmessage = (event) => {
