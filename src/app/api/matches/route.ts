e82882e38c21c197da07c320762df34f2c906cab
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-04 17:38:54 +0600

    chore: update handling of json file

--- src/app/api/matches/route.ts
+++ src/app/api/matches/route.ts
@@ -1,18 +1,22 @@
-import fs from "fs";
-import path from "path";
+import axios from "axios";
+// import fs from "fs";
+// import path from "path";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const page = searchParams.get("page") as string;
 
   try {
-    const filePath = path.resolve("./matches.json");
-    const matches = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    const totalItems = matches.length;
+    // const filePath = path.resolve("./matches.json");
+    // const matches = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const { data } = await axios.get(
+      process.env.NEXT_PUBLIC_JSON_URL as string
+    );
+    const totalItems = data.length;
 
     const startIdx = (parseInt(page, 10) - 1) * 10;
     const endIdx = startIdx + 10;
-    const paginatedData: any = JSON.stringify(matches.slice(startIdx, endIdx));
+    const paginatedData: any = JSON.stringify(data.slice(startIdx, endIdx));
 
     const response = JSON.stringify({ data: paginatedData, totalItems });
 
