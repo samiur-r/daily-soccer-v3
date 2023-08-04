fe0adcfa3f3b143cd15f0591c496a5cd5bc1af87
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-04 20:07:40 +0600

    chore: setup github actions to run cron job

--- src/utils/detectFileChange.js
+++ src/utils/detectFileChange.js
@@ -1,29 +1,19 @@
 import axios from "axios";
-// import cron from "node-cron";
 
-let previousTimestamp: any = null;
+let previousTimestamp = null;
 
 const checkFileModification = async () => {
   try {
-    const response = await axios.head(
-      process.env.NEXT_PUBLIC_JSON_URL as string
-    );
+    const response = await axios.head(process.env.NEXT_PUBLIC_JSON_URL);
     const currentTimestamp = response.headers["last-modified"];
     if (previousTimestamp && previousTimestamp !== currentTimestamp) {
       console.log("File has been modified.");
       // Here, you can trigger the logic you want to perform on file modification.
     }
     previousTimestamp = currentTimestamp;
-  } catch (error: any) {
+  } catch (error) {
     console.error("Error while fetching the file:", error.message);
   }
 };
 
-// const startFileModificationCheck = () => {
-//   // Schedule the cron job to run every hour
-//   cron.schedule("0 * * * *", () => {
-//     checkFileModification();
-//   });
-// };
-
-export default checkFileModification();
+checkFileModification();
