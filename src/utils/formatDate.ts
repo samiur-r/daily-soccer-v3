80789b8de5ed6753b757b4e91e75790376b61a00
Randy <randy.esperben@gmail.com>
2023-08-08 19:11:46 +0200

    Fix "In play" match

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -12,7 +12,11 @@ const formatDate = (dateString: string) => {
   };
 
   if (matchDate.hasSame(now, "day")) {
-    if (matchDate.diff(now, "minutes").minutes <= 90) {
+    const differenceInMinutes = matchDate.diff(now, "minutes").minutes;
+
+    // Si la diferencia en minutos es >=0 (partido aún no ha comenzado) y <90 (menos de una hora y media), el partido está en juego.
+    // Si la diferencia es negativa pero > -90, significa que el partido ya comenzó pero aún no ha terminado.
+    if (differenceInMinutes < 0 && differenceInMinutes > -90) {
       return "En juego";
     } else {
       return `Hoy ${matchDate.toFormat("HH:mm")}`;
