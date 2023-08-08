5e5b08c90797f6d3027c54fba42187d141fcd468
Randy <randy.esperben@gmail.com>
2023-08-08 17:17:05 +0200

    Sport schema

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,23 +10,26 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ data }) => {
   return (
-    <div className="flex shadow-lg border bg-white mb-2">
+    <div className="flex shadow-lg border bg-white mb-2" itemScope itemType="http://schema.org/SportsEvent">
       <div className="p-5 flex-1">
-        <p className="flex justify-end text-xs md:text-base" suppressHydrationWarning>
+        <time className="flex justify-end text-xs md:text-base" 
+        suppressHydrationWarning 
+        itemProp="startDate"
+        >
           {formatDate(data.Date)}
-        </p>
-        <h3 className="text-sm md:text-xl font-semibold italic">
+        </time>
+        <h3 className="text-sm md:text-xl font-semibold italic" itemProp="name">
           {data.Competition.Name}
         </h3>
         <div className="flex gap-3 md:text-3xl font-bold mt-3">
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center" itemScope itemType="http://schema.org/SportsTeam">
             {/* <Image src="/barcelona.png" width={30} height={30} alt="icon" /> */}
-            <h2 className="uppercase font-normal">{data.LocalTeam.Name}</h2>
+            <h2 className="uppercase font-normal" itemProp="name">{data.LocalTeam.Name}</h2>
           </div>
           <p>-</p>
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center" itemScope itemType="http://schema.org/SportsTeam">
             {/* <Image src="/barcelona.png" width={30} height={30} alt="icon" /> */}
-            <h2 className="uppercase font-normal">{data.AwayTeam.Name}</h2>
+            <h2 className="uppercase font-normal" itemProp="name">{data.AwayTeam.Name}</h2>
           </div>
         </div>
         <div className="flex flex-row gap-6 justify-end mt-3">
@@ -39,7 +42,8 @@ const Card: React.FC<CardProps> = ({ data }) => {
                 src={`/img/${channel.Image}`}
                 width={32}
                 height={32}
-                alt={channel.Name} />
+                alt={channel.Name}
+                itemProp="broadcastOfEvent" />
             ))}
         </div>
         <div className="flex gap-2 md:justify-end mt-5">
