11f1b5eada9973393eba87cc27a7cd1cc578c2c2
Randy <randy.esperben@gmail.com>
2023-08-10 15:16:48 +0200

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -64,7 +64,7 @@ const Card: React.FC<CardProps> = ({ data }) => {
         {data.Channels &&
           data.Channels.length &&
           data.Channels.map((channel) => (
-            <div itemProp="location" itemScope itemType="https://schema.org/Place">
+            <div itemProp="location" itemScope itemType="https://schema.org/Place" key={channel.Id}>
               <meta itemProp="name" content={channel.Name} />
             </div>
           ))}
