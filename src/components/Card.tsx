59a8f181f562a318e12dffccd4dc7ad7d0d8a694
Randy <randy.esperben@gmail.com>
2023-08-10 14:51:35 +0200

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,27 +10,26 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ data }) => {
   return (
-    <div className="flex shadow border bg-white mb-3" itemScope itemType="http://schema.org/SportsEvent">
+    <div className="flex shadow border bg-white mb-3">
       <div className="p-5 flex-1">
         <time className="flex justify-end text-xs md:text-base"
           suppressHydrationWarning
-          itemProp="startDate"
           dateTime={data.Date}
         >
           {formatDate(data.Date)}
         </time>
-        <h3 className="text-sm md:text-xl font-semibold italic" itemProp="name">
+        <h3 className="text-sm md:text-xl font-semibold italic">
           {data.Competition.Name}
         </h3>
         <div className="flex gap-3 text-2xl md:text-3xl font-bold mt-3">
           <div className="flex gap-2 items-center">
             {/* <Image src="/barcelona.png" width={30} height={30} alt="icon" /> */}
-            <h2 className="uppercase font-normal" itemProp="homeTeam">{data.LocalTeam.Name}</h2>
+            <h2 className="uppercase font-normal">{data.LocalTeam.Name}</h2>
           </div>
           <p>-</p>
           <div className="flex gap-2 items-center">
             {/* <Image src="/barcelona.png" width={30} height={30} alt="icon" /> */}
-            <h2 className="uppercase font-normal" itemProp="awayTeam">{data.AwayTeam.Name}</h2>
+            <h2 className="uppercase font-normal">{data.AwayTeam.Name}</h2>
           </div>
         </div>
         <div className="flex flex-row gap-6 justify-end mt-3">
@@ -44,7 +43,7 @@ const Card: React.FC<CardProps> = ({ data }) => {
                 height={32}
                 alt={channel.Name}
                 title={channel.Name}
-                itemProp={channel.Name} />
+                />
             ))}
         </div>
       </div>
