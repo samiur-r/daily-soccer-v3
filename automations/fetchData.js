1dc519e065bbb5d1275df7edfc9e446df21b0077
Randy <randy.esperben@gmail.com>
2023-08-11 15:12:14 +0200

    .

--- automations/fetchData.js
+++ automations/fetchData.js
@@ -1,98 +1,106 @@
 // Automation for GitHub Actions.
 
-const axios = require('axios');
-const AWS = require('aws-sdk');
+const axios = require("axios");
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
-
 
 // ******************************* //
 // Competition IDs to be filtered //
 const competitionIdsToFilter = [
-    3312, // La Liga EA Sports
-    3313, // LaLiga Hypermotion
-    122,  // Supercopa de España
-    113,  // Supercopa de Europa
-    57,   // MLS
-    2     // Premier League
+  3312, // La Liga EA Sports
+  3313, // LaLiga Hypermotion
+  122, // Supercopa de España
+  113, // Supercopa de Europa
+  57, // MLS
+  2, // Premier League
 ];
 // ******************************* //
 // ******************************* //
 
 /* Clean channel names */
 function updateChannelName(data) {
-    data.Channels.forEach(channel => {
-        if (channel.Name === "Amazon Prime Video (Prueba gratis)") {
-            channel.Name = "Amazon Prime Video";
-        }
-        if (channel.Name === "DAZN (Regístrate)") {
-            channel.Name = "DAZN";
-        }
-        if (channel.Name === "GOL PLAY (Síguelo en directo)") {
-            channel.Name = "GOL PLAY";
+  data.Channels.forEach((channel) => {
+    if (channel.Name === "Amazon Prime Video (Prueba gratis)") {
+      channel.Name = "Amazon Prime Video";
+    }
+    if (channel.Name === "DAZN (Regístrate)") {
+      channel.Name = "DAZN";
+    }
+    if (channel.Name === "GOL PLAY (Síguelo en directo)") {
+      channel.Name = "GOL PLAY";
+    }
+  });
+}
+
+function updateImageExtension(data) {
+    data.Competition.forEach((competition) => {
+        if (competition.Image === "20130727123206-espana.png") {
+            competition.Image = "laliga_espana.png";
         }
     });
 }
 
 const EventsData = {
-    method: 'GET',
-    url: 'https://wosti-futbol-tv-spain.p.rapidapi.com/api/Events',
-    headers: {
-        'X-RapidAPI-Key': process.env.X_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'wosti-futbol-tv-spain.p.rapidapi.com'
-    }
+  method: "GET",
+  url: "https://wosti-futbol-tv-spain.p.rapidapi.com/api/Events",
+  headers: {
+    "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": "wosti-futbol-tv-spain.p.rapidapi.com",
+  },
 };
 
 const CompetitionsData = {
-    method: 'GET',
-    url: 'https://wosti-futbol-tv-spain.p.rapidapi.com/api/Competitions',
-    headers: {
-        'X-RapidAPI-Key': process.env.X_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'wosti-futbol-tv-spain.p.rapidapi.com'
-    }
+  method: "GET",
+  url: "https://wosti-futbol-tv-spain.p.rapidapi.com/api/Competitions",
+  headers: {
+    "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": "wosti-futbol-tv-spain.p.rapidapi.com",
+  },
 };
 
 (async () => {
-    try {
+  try {
+    // Get Events
+    const responseEvents = await axios.request(EventsData);
+    const filteredEvents = responseEvents.data.filter((item) =>
+      competitionIdsToFilter.includes(item.Competition.Id)
+    );
+    filteredEvents.forEach((event) => {
+      updateChannelName(event);
+      updateImageExtension(event);
+    });
 
-        // Get Events
-        const responseEvents = await axios.request(EventsData);
-        const filteredEvents = responseEvents.data.filter(item =>
-            competitionIdsToFilter.includes(item.Competition.Id)
-        );
-        filteredEvents.forEach(event => updateChannelName(event));
+    const dataEvents = JSON.stringify(filteredEvents, null, 4);
 
-        const dataEvents = JSON.stringify(filteredEvents, null, 4);
+    var paramsEvents = {
+      Bucket: "dondelodanhbmecdrfz62tpo3f89htjtgb4kuu4zx5t8idyjdphj9xnj8gjb",
+      Key: "events_y473sycnsryug46z7vbw4xhjc2238pq2nzicw5vh6h8gypgzaw.json",
+      Body: dataEvents,
+    };
 
-        var paramsEvents = {
-            Bucket: "dondelodanhbmecdrfz62tpo3f89htjtgb4kuu4zx5t8idyjdphj9xnj8gjb",
-            Key: "events_y473sycnsryug46z7vbw4xhjc2238pq2nzicw5vh6h8gypgzaw.json",
-            Body: dataEvents
-        };
-        
-        await s3.upload(paramsEvents).promise();
+    await s3.upload(paramsEvents).promise();
 
+    // Get Competitions
+    const responseCompetitions = await axios.request(CompetitionsData);
+    const filteredCompetitions = responseCompetitions.data.filter((item) =>
+      competitionIdsToFilter.includes(item.Id)
+    );
 
-        // Get Competitions
-        const responseCompetitions = await axios.request(CompetitionsData);
-        const filteredCompetitions = responseCompetitions.data.filter(item =>
-            competitionIdsToFilter.includes(item.Id)
-        );
+    const dataCompetitions = JSON.stringify(filteredCompetitions, null, 4);
 
-        const dataCompetitions = JSON.stringify(filteredCompetitions, null, 4);
-
-        var paramsCompetitions = {
-            Bucket: "dondelodanhbmecdrfz62tpo3f89htjtgb4kuu4zx5t8idyjdphj9xnj8gjb",
-            Key: "competitions_y473sycnsryug46z7vbw4xhjc2238pq2nzicw5vh6h8gypgzaw.json",
-            Body: dataCompetitions
-        };
-        await s3.upload(paramsCompetitions).promise();
-
-    } catch (error) {
-        console.error(error);
-    }
+    var paramsCompetitions = {
+      Bucket: "dondelodanhbmecdrfz62tpo3f89htjtgb4kuu4zx5t8idyjdphj9xnj8gjb",
+      Key:
+        "competitions_y473sycnsryug46z7vbw4xhjc2238pq2nzicw5vh6h8gypgzaw.json",
+      Body: dataCompetitions,
+    };
+    await s3.upload(paramsCompetitions).promise();
+  } catch (error) {
+    console.error(error);
+  }
 })();
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,10 +13,10 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ data }) => {
   return (
-   
     <section className="flex shadow border bg-white mb-3">
       <div className="p-5 flex-1">
-        <time className="flex justify-end text-xs md:text-base"
+        <time
+          className="flex justify-end text-xs md:text-base"
           suppressHydrationWarning
           dateTime={data.Date}
         >
@@ -24,19 +24,25 @@ const Card: React.FC<CardProps> = ({ data }) => {
         </time>
         <p className="text-sm md:text-lg font-normal">
           {data.Competition.Name}
-          <Image src={`/img/${data.Competition.Image}`} width={32} height={32} alt={data.Competition.Name} />
+          <Image
+            src={`/img/${data.Competition.Image}`}
+            width={32}
+            height={32}
+            alt={data.Competition.Name}
+          />
         </p>
         <div className="flex gap-3 text-2xl md:text-3xl font-bold">
           <div className="flex gap-2 items-center">
             {/* <Image src="/barcelona.png" width={30} height={30} alt="icon" /> */}
-            <h2 className={`${oswaldFont.className} uppercase font-normal`}>{data.LocalTeam.Name} - {data.AwayTeam.Name}</h2>
+            <h2 className={`${oswaldFont.className} uppercase font-normal`}>
+              {data.LocalTeam.Name} - {data.AwayTeam.Name}
+            </h2>
           </div>
         </div>
         <div className="flex flex-row gap-6 justify-end mt-3">
           {data.Channels &&
             data.Channels.length &&
             data.Channels.map((channel) => (
-              
               <Image
                 key={channel.Id}
                 src={`/img/${channel.Image}`}
@@ -50,27 +56,48 @@ const Card: React.FC<CardProps> = ({ data }) => {
       </div>
 
       <div itemScope itemType="https://schema.org/SportsEvent">
-        <meta itemProp="name" content={`${data.LocalTeam.Name} - ${data.AwayTeam.Name}`} />
-        <meta itemProp="description" content={`Partido de fútbol entre ${data.LocalTeam.Name} - ${data.AwayTeam.Name} - ${formatDate(data.Date)} por ${data.Competition.Name}`} />
+        <meta
+          itemProp="name"
+          content={`${data.LocalTeam.Name} - ${data.AwayTeam.Name}`}
+        />
+        <meta
+          itemProp="description"
+          content={`Partido de fútbol entre ${data.LocalTeam.Name} - ${
+            data.AwayTeam.Name
+          } - ${formatDate(data.Date)} por ${data.Competition.Name}`}
+        />
         <meta itemProp="url" content="https://www.dondelodan.com/" />
         <meta itemProp="startDate" content={data.Date} />
         <meta itemProp="duration" content="PT1H30M" />
         <meta itemProp="organizer" content={data.Competition.Name} />
-        <div itemProp="homeTeam" itemScope itemType="https://schema.org/SportsTeam">
+        <div
+          itemProp="homeTeam"
+          itemScope
+          itemType="https://schema.org/SportsTeam"
+        >
           <meta itemProp="name" content={data.LocalTeam.Name} />
         </div>
-        <div itemProp="awayTeam" itemScope itemType="https://schema.org/SportsTeam">
+        <div
+          itemProp="awayTeam"
+          itemScope
+          itemType="https://schema.org/SportsTeam"
+        >
           <meta itemProp="name" content={data.AwayTeam.Name} />
         </div>
         {data.Channels &&
           data.Channels.length &&
           data.Channels.map((channel) => (
-            <div itemProp="location" itemScope itemType="https://schema.org/Place" key={channel.Id}>
+            <div
+              itemProp="location"
+              itemScope
+              itemType="https://schema.org/Place"
+              key={channel.Id}
+            >
               <meta itemProp="name" content={channel.Name} />
             </div>
           ))}
       </div>
-    </section >
+    </section>
   );
 };
 
