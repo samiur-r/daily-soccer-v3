80005ca88477800d9eede690f387554349ccb064
Randy <randy.esperben@gmail.com>
2023-08-11 11:03:17 +0200

    Revert "Update next.config.js"
    This reverts commit f7a3ff138e17479c090b62f908bd14682ae6e8a2.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/_next/image(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable', // Esto permitirá que la imagen se almacene en caché durante un año
-                    },
-                ],
-            },
-        ]
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
