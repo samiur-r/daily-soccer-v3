5865a282f61d4c63bc8d56234bf614538043764a
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-14 23:51:26 +0600

    chore: test fix of hydration error

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -106,7 +106,7 @@ const MatchList: React.FC<MatchListProps> = ({
       {Object.entries(categorizeMatchesByDate(matchList)).map(
         ([date, dateMatches]) => (
           <div key={date} suppressHydrationWarning={true}>
-            <p className="text-md font-normal mb-2 mt-6">{date}</p>
+            <p className="text-md font-normal mb-2 mt-6" suppressHydrationWarning={true}>{date}</p>
             {dateMatches.map((match) => (
               <Card key={match.Id} data={match} />
             ))}
