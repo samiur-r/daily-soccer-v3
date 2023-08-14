30e01369de50f626811caffb30e65c182477825b
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-14 22:47:50 +0600

    chore: test fix of hydration error

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -103,17 +103,16 @@ const MatchList: React.FC<MatchListProps> = ({
         </h1>
       </div>
 
-      {Object.entries(categorizeMatchesByDate(matchList)).map(
+      {/* {Object.entries(categorizeMatchesByDate(matchList)).map(
         ([date, dateMatches]) => (
-          // <div key={date} suppressHydrationWarning>
-          //   <p className="text-md font-normal mb-2 mt-6">{date}</p>
-          //   {dateMatches.map((match) => (
-          //     <Card key={match.Id} data={match} />
-          //   ))}
-          // </div>
-          <div key={Math.random()}>a</div>
+          <div key={date}>
+            <p className="text-md font-normal mb-2 mt-6">{date}</p>
+            {dateMatches.map((match) => (
+              <Card key={match.Id} data={match} />
+            ))}
+          </div>
         )
-      )}
+      )} */}
       {matchList.length < totalItems && (
         <button
           className="w-full md:max-w-max w-100 mx-auto mt-5 flex gap-3 text-2xl justify-center items-center bg-emerald-800 hover:bg-emerald-700 text-white font-regular hover:text-white py-2 px-4 border border-emerald-800 hover:border-transparent rounded"
