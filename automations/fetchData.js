6aef0923e98eae69eba4c6a7354a6cad8d42369e
Randy <randy.esperben@gmail.com>
2023-08-14 15:55:50 +0200

    Update fetchData.js

--- automations/fetchData.js
+++ automations/fetchData.js
@@ -18,11 +18,10 @@ const competitionIdsToFilter = [
   113, // Supercopa de Europa
   57, // MLS
   2, // Premier League
-  // 12, // Francie Ligue 1
-  // 13, // Serie A Italiana
-  // 25, // Primera División Argentina
+  12, // Francie Ligue 1
+  13, // Serie A Italiana
+  25, // Primera División Argentina
   // 1373 // FIFA Copa Mundial Femenina
-
 ];
 // ******************************* //
 // ******************************* //
