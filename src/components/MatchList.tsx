6265459ebdcad6d7cb5c04df6055d8d53ac3cc13
“samiur-r” <samiurrahman.dev@gmail.com>
2023-08-14 22:34:14 +0600

    chore: test fix of hydration error

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -88,7 +88,7 @@ const MatchList: React.FC<MatchListProps> = ({
   };
 
   return (
-    <div className="flex-1 w-full max-w-5xl" suppressHydrationWarning>
+    <div className="flex-1 w-full max-w-5xl">
       <div className="flex gap-5 items-center">
         {titleImage && (
           <Image
@@ -103,7 +103,7 @@ const MatchList: React.FC<MatchListProps> = ({
         </h1>
       </div>
 
-      {Object.entries(categorizeMatchesByDate(matchList)).map(
+      {/* {Object.entries(categorizeMatchesByDate(matchList)).map(
         ([date, dateMatches]) => (
           <div key={date}>
             <p className="text-md font-normal mb-2 mt-6">{date}</p>
@@ -112,7 +112,7 @@ const MatchList: React.FC<MatchListProps> = ({
             ))}
           </div>
         )
-      )}
+      )} */}
       {matchList.length < totalItems && (
         <button
           className="w-full md:max-w-max w-100 mx-auto mt-5 flex gap-3 text-2xl justify-center items-center bg-emerald-800 hover:bg-emerald-700 text-white font-regular hover:text-white py-2 px-4 border border-emerald-800 hover:border-transparent rounded"
